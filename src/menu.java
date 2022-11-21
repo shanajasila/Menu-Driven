00598d3579e9ebf9c9f80580125db6ea0dad35d7
shanajasila <shanajasilam@gmail.com>
2022-11-21 12:33:36 +0530

    created

--- src/menu.java
+++ src/menu.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class menu {
+
+    public static void main(String args[]){
+            while (true){
+                System.out.println("Enter your option:");
+                System.out.println("1.Largest among three numbers\n2.smallest among three numbers\n3.Prime or not\n4.Even or not\n5.Reverse\6.Exit");
+                Scanner sc=new Scanner(System.in);
+                int menu= sc.nextInt();
+                switch (menu){
+
+
+                }
+            }
+    }
 
 }
