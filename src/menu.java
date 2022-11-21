a00058b0bf68761590255bddeec9eb88fb959da3
shanajasila <shanajasilam@gmail.com>
2022-11-21 13:16:08 +0530

    prime

--- src/menu.java
+++ src/menu.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class menu {
 
     public static void main(String args[]){
+            int flag=0;
             while (true){
                 System.out.println("\nEnter your option:");
-                System.out.println("\n1.Largest among three numbers\n2.smallest among three numbers\n3.Prime or not\n4.Even or not\n5.Reverse\6.Exit");
+                System.out.println("\n1.Largest among three numbers\n2.smallest among three numbers\n3.Prime or not\n4.Even or not\n5.Reverse\n6.Exit");
                 Scanner sc=new Scanner(System.in);
                 int menu= sc.nextInt();
                 switch (menu){
@@ -23,6 +24,24 @@ public class menu {
                         int c1= sc.nextInt();
                         int s=((a1<b1)?(a1<c1?a1:c1):(b1<c1?b1:c1));
                         System.out.println("The largest Number is: "+s);
+                    case 3:
+                        System.out.println("\nEnter a number");
+                        int a2= sc.nextInt();
+                        if(a2==0||a2==1){
+                            System.out.println("Not a prime number");
+                        }
+                        for(int i=2;i<=a2/2;i++){
+                            if(a2%i==0){
+                                flag=1;
+                                break;
+                            }
+                        }
+                        if (flag==1) {
+                            System.out.println(a2 + " Not a prime number");
+                        }
+                        else{
+                            System.out.println(a2+" Is a prime number");
+                        }
 
                 }
             }
